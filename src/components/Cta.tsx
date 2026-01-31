86655c4f2d25e971c905f275f6d3f87b9fd72ea9
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-01-31 18:02:27 -0300

    fix: adjust title

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -5,7 +5,7 @@ function CTA() {
     <section className="flex justify-center items-center xl:h-auto">
       <div className="xl:w-304.75 xl:h-92.5 flex flex-col xl:flex-row justify-between gap-20 xl:gap-auto items-center xl:bg-[#343434] xl:rounded-[33px]">
         <div className="xl:ml-[10%] flex flex-col justify-center items-center xl:items-start gap-10 xl:gap-5">
-          <h1 className="text-[52px] text-white font-light text-center leading-16 w-80 xl:w-100">
+          <h1 className="text-[52px] text-white font-light text-center xl:text-start leading-16 w-80 xl:w-120">
             OpenType feature and Variable fonts
           </h1>
           <button className="text-white bg-[#4452FE] text-sm px-15 h-11">
